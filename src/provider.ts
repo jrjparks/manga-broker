760f5f4e997dbd7d60b76c7e7c70336a65a18a9f
James R. Parks <jarijop@gmail.com>
2017-06-24 04:49:17 -0700

    Added helper functions

--- src/provider.ts
+++ src/provider.ts
@@ -1,16 +1,24 @@
+/* tslint:disable:max-classes-per-file */
 import { CloudKicker } from "cloudkicker";
 import { Url } from "url";
-import { ScoredCache } from "./cache/ScoredCache";
 
-import { IChapter } from "./models/chapter";
-import { IDetails } from "./models/details";
-import { ISearchOptions, ISearchResults } from "./models/search";
-import { ISource } from "./models/source";
+import {
+  ScoredCache,
+} from "./cache";
+import {
+  IChapter,
+  IDetails,
+  ISearchOptions,
+  ISearchResults,
+  ISource,
+  ProviderType,
+} from "./models";
 
 export interface IProvider {
   readonly is: string;
   readonly baseURL: Url;
-  search(title: string, options?: ISearchOptions): Promise<ISearchResults>;
+  readonly provides: ProviderType;
+  search(title: string, options ?: ISearchOptions): Promise<ISearchResults>;
   details(source: ISource): Promise<IDetails>;
 }
 
@@ -19,9 +27,40 @@ export interface ISourceProvider extends IProvider {
   pages(source: ISource): Promise<ISource[]>;
 }
 
+export interface INovelProvider extends IProvider {
+  chapters(source: ISource): Promise<IChapter[]>;
+  chapter(source: ISource): Promise<string>;
+}
+
 export interface IAuthentableProvider extends IProvider {
   authenticate(username: string, password: string): Promise<this>;
   deauthenticate(): Promise<this>;
+}
+
+export function isProvider(provider: IProvider): provider is IProvider {
+  return (provider as IProvider) &&
+    (provider as IProvider).is !== undefined &&
+    (provider as IProvider).baseURL !== undefined &&
+    (provider as IProvider).search !== undefined &&
+    (provider as IProvider).details !== undefined;
+}
+
+export function isSourceProvider(provider: IProvider): provider is ISourceProvider {
+  return isProvider(provider) &&
+    (provider as ISourceProvider).chapters !== undefined &&
+    (provider as ISourceProvider).pages !== undefined;
+}
+
+export function isNovelProvider(provider: IProvider): provider is INovelProvider {
+  return isProvider(provider) &&
+    (provider as INovelProvider).chapters !== undefined &&
+    (provider as INovelProvider).chapter !== undefined;
+}
+
+export function isAuthentableProvider(provider: IProvider): provider is IAuthentableProvider {
+  return isProvider(provider) &&
+    (provider as IAuthentableProvider).authenticate !== undefined &&
+    (provider as IAuthentableProvider).deauthenticate !== undefined;
 }
 
 export class ProviderCore {
@@ -33,7 +72,7 @@ export class ProviderCore {
     this.cloudkicker = cloudkicker || new CloudKicker();
   }
 
-  public clearCache(): void {
+    public clearCache(): void {
     this.searchCache.clear();
   }
 }
