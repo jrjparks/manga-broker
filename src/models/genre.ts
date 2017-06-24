748cba6b7bf08afcb6e6a5c8d1c2cbf311d88f7c
James R. Parks <jarijop@gmail.com>
2017-06-24 04:48:12 -0700

    Added genre descriptions

--- src/models/genre.ts
+++ src/models/genre.ts
@@ -1,52 +1,160 @@
+// tslint:disable:max-line-length
 export enum Genre {
   Unknown = 0,
+
+  /** A work typically depicting fighting, violence, chaos, and fast paced motion. */
   Action = 1,
+
+  /** Contains content that is suitable only for adults. Titles in this category may include prolonged scenes of intense violence and/or graphic sexual content and nudity. */
   Adult,
+
+  /** If a character in the story goes on a trip or along that line, your best bet is that it is an adventure manga. Otherwise, it's up to your personal prejudice on this case. */
   Adventure,
+
+  /** A comic that has won an award. The award must be given by an acknowledged awarding body. */
   AwardWinning,
+
+  /** A dramatic work that is light and often humorous or satirical in tone and that usually contains a happy resolution of the thematic conflict. */
   Comedy,
+
+  /**  */
   Comic,
+
+  /** A comic based on cooking. */
   Cooking,
+
+  /**  */
   Demons,
+
+  /** Fan based work inspired by official anime or manga. For MangaUpdates, original works DO NOT fall under this category. */
   Doujinshi,
+
+  /** A work meant to bring on an emotional response, such as instilling sadness or tension. */
   Drama,
+
+  /** Possibly the line between hentai and non-hentai, ecchi usually refers to fanservice put in to attract a certain group of fans. */
   Ecchi,
+
+  /** Anything that involves, but not limited to, magic, dream world, and fairy tales. */
   Fantasy,
+
+  /** A comic-strip format, generally consists of gag comic strips within four panels of equal size ordered from top to bottom. */
   FourKoma,
+
+  /** Girls dressing up as guys, guys dressing up as girls.. Guys turning into girls, girls turning into guys.. I think you get the picture. */
   GenderBender,
+
+  /** A series involving one male character and many female characters (usually attracted to the male character). A Reverse Harem is when the genders are reversed. */
   Harem,
+
+  /** Adult sexual content in an illustrated form where the FOCUS of the manga is placed on sexually graphic acts. */
   Hentai,
+
+  /** Having to do with old or ancient times. */
   Historical,
+
+  /** A painful emotion of fear, dread, and abhorrence; a shuddering with terror and detestation; the feeling inspired by something frightful and shocking. */
   Horror,
+
+  /** Literally "Woman". Targets women 18-30. Female equivalent to seinen. Unlike shoujo the romance is more realistic and less idealized. The storytelling is more explicit and mature. */
   Josei,
+
+  /** Representing a sexual attraction to young or under-age girls. */
   Lolicon,
+
+  /**  */
   Magic,
+
+  /** As the name suggests, anything martial arts related. Any of several arts of combat or self-defense, such as aikido, karate, judo, or tae kwon do, kendo, fencing, and so on and so forth. */
   MartialArts,
+
+  /** Contains subject matter which may be too extreme for people under the age of 17. Titles in this category may contain intense violence, blood and gore, sexual content and/or strong language. */
   Mature,
+
+  /** A work involving and usually concentrating on all types of large robotic machines. */
   Mecha,
+
+  /** Comics about doctors, nurses, etc. around the field of medicine. */
   Medical,
+
+  /**  */
   Military,
+
+  /** A music themed story. */
   Music,
+
+  /** Usually an unexplained event occurs, and the main protagonist attempts to find out what caused it. */
   Mystery,
+
+  /** A one-shot is a comic book that is a pilot or a stand-alone story created as a single issue, rather than the more typical series format. */
   Oneshot,
+
+  /** Usually deals with the philosophy of a state of mind, in most cases detailing abnormal psychology. */
   Psychological,
+
+  /** Any love related story. We will define love as between man and woman in this case. Other than that, it is up to your own imagination of what love is. */
   Romance,
+
+  /** Having a major setting of the story deal with some type of school. */
   SchoolLife,
+
+  /** Short for science fiction, these works involve twists on technology and other science related phenomena which are contrary or stretches of the modern day scientific world. */
   Scifi,
+
+  /** Seinen means "young Man." Manga and anime that specifically targets young adult males around the ages of 18 to 25 are seinen titles. The stories in seinen works appeal to university students and those in the working world. Typically the story lines deal with the issues of adulthood. */
   Seinen,
+
+  /** Representing a sexual attraction to young or under-age boys. */
   Shotacon,
+
+  /** A work intended and primarily written for females. Usually involves a lot of romance and strong character development. */
   Shoujo,
+
+  /** Often synonymous with yuri, this can be thought of as somewhat less extreme. "Girl's Love", so to speak. */
   ShoujoAi,
+
+  /** A work intended and primarily written for males. These works usually involve fighting and/or violence. */
   Shounen,
+
+  /** Often synonymous with yaoi, this can be thought of as somewhat less extreme. "Boy's Love", so to speak. */
   ShounenAi,
+
+  /** As the name suggests, this genre represents day-to-day tribulations of one/many character(s). These challenges/events could technically happen in real life and are often -if not all the time- set in the present timeline in a world that mirrors our own. */
   SliceOfLife,
+
+  /** Deals with series that are considered profane or offensive, particularly with regards to sexual content. */
   Smut,
+
+  /** As the name suggests, anything sports related. Baseball, basketball, hockey, soccer, golf, and racing just to name a few. */
   Sports,
+
+  /**  */
   SuperPower,
+
+  /** Usually entails amazing and unexplained powers or events which defy the laws of physics. */
   Supernatural,
+
+  /** Contains events resulting in great loss and misfortune. */
   Tragedy,
+
+  /**  */
   Vampire,
+
+  /** A webtoon is a comic strip that is distributed via the Web. Usually webtoons are drawn in full color and read in one long strip per chapter. */
   Webtoon,
+
+  /** This is a genre specific to Chinese novels. It literally means "Martial Hero", dealing with martial arts and chivalry. Main character can't become an immortal, but rather stays human. */
+  Wuxia,
+
+  /** Xianxia is a story with cultivation that can lead to a human becoming immortal through Taoist (in most cases) ways. If it doesn't mention the dao, odds are that it isn't a Xianxia. */
+  Xianxia,
+
+  /** Eastern fantasy - This term is use for those fiction set in a eastern culture background (Eastern Fantasy). In Xuanhuan you get five basic element (metal, wood, water, fire and earth), Chinese style gods, powerful animal monster like fox and tiger who can transform to human look, etc... It's like a mishmash of different elements and sometimes Xianxia-like elements are present. It's easy to confuse Xuanhuan with Xianxia. */
+  Xuanhuan,
+
+  /** This work usually involves intimate relationships between men. */
   Yaoi,
+
+  /** This work usually involves intimate relationships between women. */
   Yuri,
 }
